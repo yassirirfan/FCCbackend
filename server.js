fb30980a3a7ff0c7533376c45498c48cde5b18b9
Yassir Irfan <yassirirfan7@gmail.com>
2021-06-05 20:36:33 +0530

    initial

--- server.js
+++ server.js
@@ -24,7 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+ //START
+app.get("/api/:timestamp", (req, res) => {
+  let timestamp = req.params.timestamp;
+  if(timestamp.match(/\d{5,}/)){
+    timestamp =+ timestamp;
+  }
+  let date = new Date(timestamp);
+  if(date.toUTCString() == "Invalid Date"){
+    res.json({error:date.toUTCString})
+  }
+  res.json( {unix : date.valueOf(), utc: date.toUTCString()} )
+});
 
+app.get("/api/", (req, res) => {
+let date = new Date();
+res.json( {unix : date.valueOf(), utc: date.toUTCString()} )
+});
+//END
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
